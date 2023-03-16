e9c2a355111e320ff7e0dd90e43d4faabaf66d55
Daniil Komarov <worstyoungerbrother@yandex.ru>
2023-03-17 00:59:57 +0300

    first commit

---
+++ datastructures 1 lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b = 0,answer;
+
+            Console.WriteLine("Введите число натуральное число N : ");
+            a = int.Parse(Console.ReadLine());
+            if(a % 2 != 0)
+            {
+                a /= 2;
+                b += a++;
+            }
+            else
+            {
+                a /= 2;
+                b += a;
+            }
+           
+            Console.WriteLine(" Выходные данные : " + a + " " + b);
+            
+        }
+    }
+}
